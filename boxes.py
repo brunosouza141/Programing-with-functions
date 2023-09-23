efc652d3c2d3a48db3ea2bbd289e618ddce68074
Bruno Souza <brunosouza141@gmail.com>
2023-09-23 00:43:27 -0300

    1

--- boxes.py
+++
@@ -1,5 +0,0 @@
-import math
-i = int(input('The number of manufactured items'))
-t = int(input('The number of items that the user will pack per box'))
-
-print(f'For {i} items, packing {t} items in each box, you will need {math.ceil(i/t)} boxes.')
